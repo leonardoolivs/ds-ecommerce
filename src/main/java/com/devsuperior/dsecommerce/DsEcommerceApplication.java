a1507e3639b164c73f2802bc025e78c301fdb08e
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-04 14:48:51 -0300

    build: Criacao do projeto

---
+++ src/main/java/com/devsuperior/dsecommerce/DsEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsuperior/dsecommerce/DsEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
