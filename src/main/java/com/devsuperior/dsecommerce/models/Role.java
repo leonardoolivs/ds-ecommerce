957af09f4ed1b6d0892ac3fa79ae2497677fa092
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-09 16:32:40 -0300

    build: Cria a Entity de roles para dar permissao ao usuario, implementando o GrantedAuthority

---
+++ src/main/java/com/devsuperior/dsecommerce/models/Role.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dsecommerce.models;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "TB_USER")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String authority;
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Role(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
