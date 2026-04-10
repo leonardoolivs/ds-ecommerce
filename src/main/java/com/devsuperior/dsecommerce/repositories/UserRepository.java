c2dd3f6f8c560128103becc890ac2a75ffd908b4
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-10 17:46:34 -0300

    feat: Adiciona metodo para retornar um UsuarioDTO detalhe caso seja autenticado corretamento, se nao retorna email nao encontrado

--- src/main/java/com/devsuperior/dsecommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dsecommerce/repositories/UserRepository.java
@@ -1,12 +1,13 @@
 package com.devsuperior.dsecommerce.repositories;
 
-import com.devsuperior.dsecommerce.dtos.UserDTO;
+import com.devsuperior.dsecommerce.dtos.UserAuthDTO;
 import com.devsuperior.dsecommerce.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -16,5 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
             "INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id " +
             "INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id " +
             "WHERE tb_user.email = :username")
-    List<UserDTO> searchUserAndRolesByEmail(String username);
+    List<UserAuthDTO> searchUserAndRolesByEmail(String username);
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/devsuperior/dsecommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dsecommerce/services/UserService.java
@@ -1,14 +1,19 @@
 package com.devsuperior.dsecommerce.services;
 
+import com.devsuperior.dsecommerce.dtos.UserAuthDTO;
 import com.devsuperior.dsecommerce.dtos.UserDTO;
 import com.devsuperior.dsecommerce.models.Role;
 import com.devsuperior.dsecommerce.models.User;
 import com.devsuperior.dsecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,7 +25,7 @@ public class UserService implements UserDetailsService{
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        List<UserDTO> result = userRepository.searchUserAndRolesByEmail(username);
+        List<UserAuthDTO> result = userRepository.searchUserAndRolesByEmail(username);
 
         if(result.isEmpty()){
             throw new UsernameNotFoundException("User not found");
@@ -29,10 +34,30 @@ public class UserService implements UserDetailsService{
         User user = new User();
         user.setEmail(username);
         user.setPassword(result.get(0).getPassword());
-        for(UserDTO userDTO : result){
-            user.addRole(new Role(userDTO.getRoleId(), userDTO.getAuthority()));
+        for(UserAuthDTO userAuthDTO : result){
+            user.addRole(new Role(userAuthDTO.getRoleId(), userAuthDTO.getAuthority()));
         }
 
         return user;
+    }
+
+    protected User authenticated(){
+        try {
+            //Vai pegar um usuário autenticado
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return userRepository.findByEmail(username).get();
+        }
+        catch (Exception e){
+            throw new UsernameNotFoundException("Email not found");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe(){
+        User user = authenticated();
+
+        return new UserDTO(user);
     }
 }
